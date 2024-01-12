36c0aa40bbf650578304028e8a547172e9da17f1
kamillitman <kamil.litman@gmail.com>
2024-01-12 16:10:58 -0500

    google-gemini-pro

--- examples/use_completions.rs
+++ examples/use_completions.rs
@@ -1,9 +1,10 @@
 use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
+use yup_oauth2::{read_service_account_key, ServiceAccountAuthenticator};
 
 use allms::{
-    llm_models::{AnthropicModels, MistralModels, OpenAIModels},
+    llm_models::{AnthropicModels, GoogleModels, MistralModels, OpenAIModels},
     Completions,
 };
 
@@ -21,7 +22,7 @@ async fn main() {
 
     // Example context and instructions
     let instructions =
-        "Translate this exact English sentence to all the languages in the response type: \"Hi, how are you?\"";
+        "Translate the following English sentence to all the languages in the response type: Rust is best for working with LLMs";
 
     // Get answer using OpenAI
     let openai_api_key: String = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
@@ -60,11 +61,40 @@ async fn main() {
     let mistral_completion = Completions::new(model, &mistral_api_key, None, None);
 
     match mistral_completion
-        .debug()
         .get_answer::<TranslationResponse>(instructions)
         .await
     {
         Ok(response) => println!("Mistral response: {:?}", response),
+        Err(e) => eprintln!("Error: {:?}", e),
+    }
+
+    // Get answer using Google GeminiPro
+    let model = GoogleModels::GeminiProVertex; // Choose the model
+
+    // To authenticate Google we need to use a key associated with a GCP service account with correct grants
+    // Load your service account key from a file or an environment variable
+    let service_account_key = read_service_account_key("secrets/gcp_sa_key.json")
+        .await
+        .unwrap();
+
+    // Authenticate with your service account
+    let auth = ServiceAccountAuthenticator::builder(service_account_key)
+        .build()
+        .await
+        .unwrap();
+    let google_token = auth
+        .token(&["https://www.googleapis.com/auth/cloud-platform"])
+        .await
+        .unwrap();
+    let google_token_str = &google_token.token().unwrap();
+
+    let gemini_completion = Completions::new(model, &google_token_str, None, None);
+
+    match gemini_completion
+        .get_answer::<TranslationResponse>(instructions)
+        .await
+    {
+        Ok(response) => println!("Gemini response: {:?}", response),
         Err(e) => eprintln!("Error: {:?}", e),
     }
 }
