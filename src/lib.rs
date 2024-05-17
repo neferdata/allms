cef63cc1639b9e0ed7b613a4fac45c70618c1816
kamillitman <kamil.litman@gmail.com>
2024-05-17 10:49:58 -0400

    suppress-compiler-deprecation-warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 pub mod assistants;
 mod completions;
 mod constants;
-mod deprecated;
 mod domain;
 mod enums;
 pub mod llm_models;
 mod utils;
 
+#[allow(deprecated)]
+mod deprecated;
+
 pub use crate::completions::Completions;
+#[allow(deprecated)]
 pub use crate::deprecated::{
     OpenAI, OpenAIAssistant, OpenAIAssistantVersion, OpenAIFile, OpenAIModels,
 };
