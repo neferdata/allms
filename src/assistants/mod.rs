ef0d2096e881fb729bb72a353e7f51f1aee5a25f
Kamil Litman <97270937+kamillitman@users.noreply.github.com>
2024-08-05 11:27:30 +0200

    Documentation & bug fixes - 20240730 (#42)

--- src/assistants/mod.rs
+++ src/assistants/mod.rs
@@ -1,3 +1,6 @@
 pub mod openai;
 
-pub use openai::{OpenAIAssistant, OpenAIAssistantVersion, OpenAIFile, OpenAIVectorStore};
+pub use openai::{
+    OpenAIAssistant, OpenAIAssistantVersion, OpenAIFile, OpenAIVectorStore,
+    OpenAIVectorStoreFileCounts, OpenAIVectorStoreStatus,
+};
